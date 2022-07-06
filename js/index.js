1d91ea841477a46527b6db927f7b2ba936af7918
Andrea <acesparza05@gmail.com>
2022-07-06 12:26:36 -0500

    commit tree f8d76d339e5f35ad80c318d587a02b5d6768a704

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const baseUrl = 'http://localhost:3000/books'
+
+const getBooks = () => {
+    fetch(baseUrl)
+    .then(res => res.json())
+    .then(json => displayTitlesForBooks(json))
+}
+
+const displayTitlesForBooks = (books) => {
+    const list = document.getElementById('list');
+    books.forEach(book => {
+        const item = document.createElement('li');
+        item.textContent = book.title;
+        addListenerToTitle(item, book)
+        list.appendChild(item);
+    })
+}
+
+const addListenerToTitle = (listItem, book) => {
+    const showPanel = document.getElementById('show-panel')
+    listItem.addEventListener('click', () => {
+        showPanel.textContent = '';
+        const image = document.createElement('img');
+        image.src = book.img_url;
+        const p = document.createElement('p');
+        p.textContent = book.description;
+        showPanel.append(image, p);
+    })
+}
+
+getBooks();
